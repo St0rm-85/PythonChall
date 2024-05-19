30a3be746aa3eb994db41ca9e803f65a504a9cec
St0rm-85 <82373787+St0rm-85@users.noreply.github.com>
2024-05-19 21:51:44 +0700

    map.html solution 2 update

--- challange2/map_html_solution2.py
+++ challange2/map_html_solution2.py
@@ -1,6 +1,11 @@
+## cách này sử dụng maketrans() như hint
+
 Secret = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+swap = str.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyzab")
+
 
 result = ""
 
 
-print(result)
+print(Secret.translate(swap))
